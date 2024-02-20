9a3ae838c165c3ee1f5892fbffc3e6285a6e1bf1
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-20 14:37:21 +0100

    added all pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,20 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#list"
+
+  get "tasks/new", to: "tasks#new"
+
+  post "tasks", to: "tasks#create"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+  get "tasks/:id/edit", to: "tasks#edit"
+
+  patch "tasks/:id", to: "tasks#update"
+  
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
